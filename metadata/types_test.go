ca5ed7dc0b24e8f0f4046b564b6aa463b8f5b9d6
YoEight <yo.eight@gmail.com>
2021-09-13 00:00:44 +0200

    Add user and system stream acl tests.

--- metadata/types_test.go
+++ metadata/types_test.go
@@ -9,7 +9,7 @@ import (
 	"github.com/EventStore/EventStore-Client-Go/metadata"
 )
 
-func TestConsistentMetadataSerialization(t *testing.T) {
+func TestConsistentMetadataSerializationStreamAcl(t *testing.T) {
 	acl := metadata.AclDefault().
 		AddReadRoles("admin").
 		AddWriteRoles("admin").
@@ -23,6 +23,66 @@ func TestConsistentMetadataSerialization(t *testing.T) {
 		TruncateBefore(1).
 		MaxCount(12).
 		Acl(acl).
+		AddCustomProperty("foo", "bar")
+
+	props, err := expected.ToMap()
+
+	assert.NoError(t, err, "failed to generate a map")
+
+	bytes, err := json.Marshal(props)
+
+	assert.NoError(t, err, "failed to serialize in JSON")
+
+	var outProps map[string]interface{}
+
+	err = json.Unmarshal(bytes, &outProps)
+
+	assert.NoError(t, err, "failed to deserializing props")
+
+	meta, err := metadata.StreamMetadataFromMap(outProps)
+
+	assert.NoError(t, err, "failed to parse metadata from props")
+
+	assert.Equal(t, expected, meta, "consistency serialization failure")
+}
+
+func TestConsistentMetadataSerializationUserStreamAcl(t *testing.T) {
+	expected := metadata.StreamMetadataDefault().
+		MaxAge(2 * time.Second).
+		CacheControl(15 * time.Second).
+		TruncateBefore(1).
+		MaxCount(12).
+		Acl(metadata.UserStreamAcl).
+		AddCustomProperty("foo", "bar")
+
+	props, err := expected.ToMap()
+
+	assert.NoError(t, err, "failed to generate a map")
+
+	bytes, err := json.Marshal(props)
+
+	assert.NoError(t, err, "failed to serialize in JSON")
+
+	var outProps map[string]interface{}
+
+	err = json.Unmarshal(bytes, &outProps)
+
+	assert.NoError(t, err, "failed to deserializing props")
+
+	meta, err := metadata.StreamMetadataFromMap(outProps)
+
+	assert.NoError(t, err, "failed to parse metadata from props")
+
+	assert.Equal(t, expected, meta, "consistency serialization failure")
+}
+
+func TestConsistentMetadataSerializationSystemStreamAcl(t *testing.T) {
+	expected := metadata.StreamMetadataDefault().
+		MaxAge(2 * time.Second).
+		CacheControl(15 * time.Second).
+		TruncateBefore(1).
+		MaxCount(12).
+		Acl(metadata.SystemStreamAcl).
 		AddCustomProperty("foo", "bar")
 
 	props, err := expected.ToMap()
